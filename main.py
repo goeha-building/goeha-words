2ea9aacddd49e69a0445072f6452d6948e03fcf8
goeha-building <mina1379@kakao.com>
2025-12-14 14:20:07 +0900

    니똥꼬윤겔라

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ import customtkinter
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-        self.geometry("400x150")
+        self.geometry("700x500")
 
         self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callbck)
         self.button.pack(padx=20, pady=20)
+
+        self.button2 = customtkinter.CTkButton(self, text="my button", command=self.button_callbck)
+        self.button2.pack(padx=20, pady=20)
+
 
     def button_callbck(self):
         print("button clicked")
