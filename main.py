baa98dd65f39305cf25cbc7af7efc46e299f7709
freebird_han <freebird_han@naver.com>
2025-12-14 13:27:33 +0900

    update

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import tkinter as tk
+import customtkinter
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("400x150")
+
+        self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callbck)
+        self.button.pack(padx=20, pady=20)
+
+    def button_callbck(self):
+        print("button clicked")
+
 
 def main():
-    print("Hello from goeha-words!")
-
+    app = App()
+    app.mainloop()
 
 if __name__ == "__main__":
     main()
