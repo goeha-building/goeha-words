745c5be5b33fb33041a52976dd689e1137ee494d
goeha-building <mina1379@kakao.com>
2025-12-14 13:27:48 +0900

    goeha

--- main.py
+++ main.py
@@ -3,6 +3,23 @@ import tkinter as tk
 def main():
     print("Hello from goeha-words!")
 
+    WINDOW_WIDTH = 800
+    WINDOW_HEIGHT = 600
+    root: tk.Tk = tk.Tk()
+    root.title("goeha-words")
+    root.geometry(f"{(WINDOW_WIDTH)}x{WINDOW_HEIGHT}")
+    canvas = tk.Canvas(
+    master=root,
+    width=WINDOW_WIDTH,
+    height=WINDOW_HEIGHT,
+    bg="white",
+    )
+    canvas.pack()
+
+    root.mainloop()
+
 
 if __name__ == "__main__":
     main()
+
+
