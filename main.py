29bab366e7bdeb7fb8c2819830449c8b407ca76d
goeha <mina1379@kakao.com>
2025-12-16 23:01:39 +0900

    오류나서 주석넣음

--- main.py
+++ main.py
@@ -249,7 +249,7 @@ class App(customtkinter.CTk):
             sticky="e",
         )
         self.update_clock()
-        self.separator.grid(row=3, column=1, padx=20, pady=(20, 5), sticky="e")
+        #self.separator.grid(row=3, column=1, padx=20, pady=(20, 5), sticky="e")
 
         # 스톱워치 ui
         self.sw_label = customtkinter.CTkLabel(
